2475a1e861b7f80c3f2d8f416b869585b81fd7ab
ryotatakasaki226 <s2222022@stu.musashino-u.ac.jp>
2024-05-25 14:37:53 +0900

    Update code with latest changes

--- setup.py
+++ setup.py
@@ -2,18 +2,26 @@ from setuptools import setup, find_packages
 
 setup(
     name='promising_stocks',
-    version='0.1.0',
-    author='ryotatakasaki',
+    version='0.2.6',
+    author='ryotatakasaki226',
     author_email='s2222022@stu.musashino-u.ac.jp',
     description='A brief description of your package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/ryotatakasaki226/pr_st.git,
+    url='https://github.com/ryotatakasaki226/pr_st.git',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'plotly',
+        'pandas_datareader',
+        'matplotlib',
+        'datetime',
     ],
     python_requires='>=3.6',
 )
